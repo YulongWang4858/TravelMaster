852ae6f9ca46b1354e3042c2d3590e0625530698
YulongWang4858 <wyl1004858369@gmail.com>
2018-11-08 14:04:50 +0800

    initial commit

---
+++ TravelMaster/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TravelMaster
+//
+//  Created by Wang Yulong on 8/11/18.
+//  Copyright © 2018 Wang Yulong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
