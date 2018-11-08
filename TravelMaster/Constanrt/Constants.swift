1d38187b0d5b9bbbb7f8d0f05e6220952c1e0b53
YulongWang4858 <wyl1004858369@gmail.com>
2018-11-08 15:53:32 +0800

    Add(SecondVC): Integrate Gmap on SecondV and Podfile inclusionC

---
+++ TravelMaster/Constanrt/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TravelMaster
+//
+//  Created by Wang Yulong on 8/11/18.
+//  Copyright © 2018 Wang Yulong. All rights reserved.
+//
+
+import Foundation
+
+struct Constants
+{
+    public static let GMAP_API_KEY = "AIzaSyA4Z_wMk2YGtxGspyKhVDyLLmrbqFr4JjY";
+}
--- TravelMaster/SecondViewController.swift
+++ TravelMaster/SecondViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // load GMap
+        GMSServices.provideAPIKey(Constants.GMAP_API_KEY);
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 1.318056, longitude: 103.908629, zoom: 20);
+        let mapView = GMSMapView.map(withFrame:CGRect.zero, camera: camera);
+        view = mapView;
     }
-
 
 }
 
